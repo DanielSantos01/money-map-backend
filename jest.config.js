16f037dbb5ddf855db871f3bddf74d1f85bba0f8
MauricioSight <mauriciosightman@gmail.com>
2022-09-28 18:39:36 -0300

    feat: config test with jest

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+/* eslint-disable max-len */
+
+module.exports = {
+  bail: true,
+
+  clearMocks: true,
+
+  testEnvironment: 'jest-environment-node',
+
+  testMatch: ['**/tests/**/*.test.ts?(x)'],
+};
