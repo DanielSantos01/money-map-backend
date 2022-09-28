6373d251a07018dc29ba7f6e6f539d587c5aa93a
PaulaPerazzo <mpperazzosampaio@gmail.com>
2022-09-28 13:00:22 -0300

    feature/jestTest: test implemented to user CRUD

---
+++ tests/helper/database.config.ts
@@ -0,0 +1,24 @@
+import { ConnectionOptions } from "typeorm";
+import { DatabaseConnection } from "./DatabaseConnection";
+import 'dotenv/config';
+import { User } from '../../src/models';
+
+const dbEntities = [User];
+
+const options: ConnectionOptions = {
+  name: 'default',
+  type: 'postgres',
+  host: process.env.DATABASE_TEST_HOST,
+  port: Number(process.env.DATABASE_TEST_PORT),
+  username: process.env.DATABASE_TEST_USER,
+  password: process.env.DATABASE_TEST_PASSWORD,
+  database: process.env.DATABASE_TEST_DB,
+  logging: false,
+  synchronize: true,
+  entities: dbEntities,
+  cli: { entitiesDir: '../src/models' },
+};
+
+const connection = new DatabaseConnection(options);
+
+export { connection };
